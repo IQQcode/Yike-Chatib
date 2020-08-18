3d3b0f4f3f24a5de0d75b7679f7206fe19cf2e9c
iqqcode <2726109782@qq.com>
2020-08-18 20:31:15 +0800

    message send client to Server

--- src/main/java/com/iqqcode/chatib/model/User.java
+++ src/main/java/com/iqqcode/chatib/model/User.java
@@ -1,25 +1,28 @@
 package com.iqqcode.chatib.model;
 
 import com.iqqcode.chatib.web.MyWebSocket;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 /**
  * @Author: iqqcode
  * @Date: 2020-08-18 07:21
  * @Description:用户实体类
  */
-
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @EqualsAndHashCode
 public class User {
 
-    //房间号id
+    //room-id
     public String id;
 
     //用户名
     public String nickname;
 
-    //该用户对应的聊天室
+    //room-聊天室
     public MyWebSocket webSocket;
 }
--- src/main/java/com/iqqcode/chatib/web/MyWebSocket.java
+++ src/main/java/com/iqqcode/chatib/web/MyWebSocket.java
@@ -1,6 +1,8 @@
 package com.iqqcode.chatib.web;
 
+import cn.hutool.core.util.StrUtil;
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.iqqcode.chatib.model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -33,13 +35,13 @@ public class MyWebSocket {
     private Session session;
 
     //用以记录用户和房间号的对应关系(sessionId,room)
-    private static HashMap<String,String> RoomForUser = new HashMap<String,String>();
+    private static HashMap<String, String> RoomForUser = new HashMap<String, String>();
 
     //用以记录房间和其中用户群的对应关系(room,List<用户>)
-    public static HashMap<String, CopyOnWriteArraySet<User>> UserForRoom = new HashMap<String,CopyOnWriteArraySet<User>>();
+    public static HashMap<String, CopyOnWriteArraySet<User>> UserForRoom = new HashMap<String, CopyOnWriteArraySet<User>>();
 
     //用以记录房间和其中用户群的对应关系(room,List<用户>)
-    public static HashMap<String,String> PwdForRoom = new HashMap<String,String>();
+    public static HashMap<String, String> PwdForRoom = new HashMap<String, String>();
 
     //用来存放必应壁纸
     public static List<String> BingImages = new ArrayList<>();
@@ -50,19 +52,20 @@ public class MyWebSocket {
 
     /**
      * 连接建立成功调用的方法
+     *
      * @param session
      */
     @OnOpen
     public void onOpen(Session session) throws IOException {
-        logger.debug("---------------------成功与{}建立连接---------------------",session.getId());
+        logger.debug("---------------------成功与{}建立连接---------------------", session.getId());
         //将局部的session给成员session
         this.session = session;
         //统计在线人数
         addOnlineCount();
-        Map<String, String> result = new HashMap<String, String> ();
+        Map<String, String> result = new HashMap<String, String>();
         //存储缓存壁纸信息
         result.put("type", "bing");
-        result.put("msg",BingImages.get(random.nextInt(BingImages.size())));
+        result.put("msg", BingImages.get(random.nextInt(BingImages.size())));
         //将当前用户上线消息推送给同房间的用户
         result.put("sendUser", "系统消息");
         result.put("id", session.getId());
@@ -83,7 +86,66 @@ public class MyWebSocket {
      */
     @OnMessage
     public void onMessage(String message, Session session) {
-
+        //将json-message转为message对象
+        Map<String, String> map = new Gson().fromJson(message, new TypeToken<HashMap<String, String>>() {
+        }.getType());
+        //容器来保存转换后的数据
+        Map<String, String> result = new HashMap<>();
+        User user = null;
+        String shiels = map.containsKey("shiels") ? map.get("shiels").toString() : null;
+        switch (map.get("type")) {
+            //消息数据
+            case "msg":
+                //获取要发送的用户
+                user = getUser(session);
+                //将数据放入容器
+                result.put("type", "msg"); //消息类型
+                result.put("msg", map.get("msg")); //消息内容
+                result.put("sendUser", user.getNickname()); //发送给谁
+                result.put("shake", map.get("shake")); //消息动画
+                break;
+            //Room数据
+            case "init":
+                //获取当前Room信息: room-id,username,room-password
+                String room = map.get("room");
+                String nick = map.get("nick");
+                String pwd = map.get("pwd");
+                if (room != null && nick != null) {
+                    user = new User(session.getId(), nick, this);
+                    //如果房间不存在，新建房间
+                    if (UserForRoom.get(room) == null) {
+                        CopyOnWriteArraySet<User> roomUsers = new CopyOnWriteArraySet<>();
+                        roomUsers.add(user);
+                        UserForRoom.put(room, roomUsers);
+                        if (StrUtil.isNotEmpty(pwd)) {
+                            PwdForRoom.put(room, pwd);
+                        }
+                        RoomForUser.put(session.getId(), room);
+                    } else {
+                        UserForRoom.get(room).add(user);
+                        RoomForUser.put(session.getId(), room);
+                    }
+                    result.put("type", "init");
+                    result.put("msg", nick + "成功加入房间");
+                    result.put("sendUser", "系统消息");
+                }
+                break;
+            //背景图片
+            case "img":
+                user = getUser(session);
+                result.put("type", "img");
+                result.put("msg", map.get("msg"));
+                result.put("sendUser", user.getNickname());
+                break;
+            case "ping":
+                return;
+        }
+        //StrUtil处理表情包字符串
+        if (StrUtil.isEmpty(shiels)) {
+            sendMessagesOther(getUsers(session), gson.toJson(result));
+        } else {
+            sendMessagesOther(getUsers(session), gson.toJson(result), shiels);
+        }
     }
 
     /**
@@ -115,12 +177,83 @@ public class MyWebSocket {
     }
 
     /**
-     * 上线消息广播
+     * 文本消息发送
+     *
      * @param message
      * @throws IOException
      */
     public void sendMessage(String message) throws IOException {
         //获取session来发送消息
         this.session.getBasicRemote().sendText(message);
+    }
+
+    /**
+     * 通过当前用户的session获取待发送的目标用户
+     * @param session
+     * @return
+     */
+    private User getUser(Session session) {
+        String room = RoomForUser.get(session.getId());
+        CopyOnWriteArraySet<User> users = UserForRoom.get(room);
+        for (User user : users) {
+            if (session.getId().equals(user.getId())) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 通过当前用户的session获取当前room内的所有用户,存入Set
+     * @param session
+     * @return
+     */
+    private CopyOnWriteArraySet<User> getUsers(Session session) {
+        String room = RoomForUser.get(session.getId());
+        CopyOnWriteArraySet<User> users = UserForRoom.get(room);
+        return users;
+    }
+
+
+    /**
+     * 给room内所有用户群发信息
+     * @param users 用户集合
+     * @param message 消息数据(无表情)
+     */
+    private void sendMessagesOther(CopyOnWriteArraySet<User> users, String message) {
+        //遍历Set中的每个用户，群发
+        for (User item : users) {
+            //不能给自己发
+            if (item.getWebSocket() != this) {
+                try {
+                    item.getWebSocket().sendMessage(message);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+
+    /**
+     * 给room内所有用户群发信息
+     * @param users 用户集合
+     * @param message 消息数据
+     * @param shiel 表情字符串
+     */
+    private void sendMessagesOther(CopyOnWriteArraySet<User> users, String message, String shiel) {
+        //将存表情包字符串的数组转为List
+        //var shiels = new Array();
+        List<String> shiels = Arrays.asList(shiel.split(","));
+        //群发消息
+        for (User item : users) {
+            if (item.getWebSocket() != this && !shiels.contains(item.getId())){
+                try {
+                    item.getWebSocket().sendMessage(message);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
